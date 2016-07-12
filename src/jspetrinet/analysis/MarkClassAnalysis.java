4611e405ba10567cab9354ebb4d739b7d5995d8a
Hiroyuki Okamura <okamu@rel.hiroshima-u.ac.jp>
2016-07-13 01:15:54 +0900

    sccのバグ修正．transientをまとめるのは難しいので諦めるか？

--- src/jspetrinet/analysis/MarkClassAnalysis.java
+++ src/jspetrinet/analysis/MarkClassAnalysis.java
@@ -1,6 +1,7 @@
 package jspetrinet.analysis;
 
 import java.io.PrintWriter;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -20,87 +21,105 @@ public class MarkClassAnalysis {
 	private final Set<MarkGroup> allMarkGroup;
 	private final Set<MarkGroup> dagMarkGroup;
 	private final Map<Mark,MarkGroup> markToGroup;
+	private final Set<Mark> dagtran;
 
 	public MarkClassAnalysis(Net net) {
 		this.net = net;
 		allMarkGroup = new HashSet<MarkGroup>();
 		markToGroup = new HashMap<Mark,MarkGroup>();
 		dagMarkGroup = new HashSet<MarkGroup>();
+		dagtran = new HashSet<Mark>();
 	}
 	
 	public void scc(Mark start, Set<Mark> allmark) {
 		// SCC: Decomposition of Strongly Connected Components by Kosaraju
 		// init
 		LinkedList<Mark> sorted = new LinkedList<Mark>();
+		LinkedList<Mark> comp = new LinkedList<Mark>();
 		LinkedList<Mark> novisited = new LinkedList<Mark>();
 		Set<Mark> visited = new HashSet<Mark>();
-		Set<Mark> reserved = new HashSet<Mark>();
 		
 		// DFS 1 pass
 		novisited.push(start);
-		reserved.add(start);
 		while (!novisited.isEmpty()) {
 			Mark m = novisited.pop();
+			if (m == null) {
+				sorted.push(comp.pop());
+				continue;
+			}
 			if (visited.contains(m)) {
-				sorted.push(m);
 				continue;
 			}
 			visited.add(m);
 
-			novisited.push(m);
+			novisited.push(null);
+			comp.push(m);
 			for (Arc a : m.getOutArc()) {
 				Mark next = (Mark) a.getDest();
-				if (!reserved.contains(next)) {
-					novisited.push(next);
-					reserved.add(next);
-				}
+				novisited.push(next);
 			}
 		}
 
 		// DFS 2 pass (reverse)
 		novisited.clear();
 		visited.clear();
-		reserved.clear();
-		while (!sorted.isEmpty()) {
-			Mark m = sorted.pop();
+		for (Mark m : sorted) {
 			if (!visited.contains(m)) {
 				MarkGroup mg = new MarkGroup(JSPetriNet.markToString(net, m));
 				novisited.push(m);
-				reserved.add(m);
 				while (!novisited.isEmpty()) {
 					Mark m2 = novisited.pop();
+					if (visited.contains(m2)) {
+						continue;
+					}
 					visited.add(m2);
 					mg.add(m2);
 					markToGroup.put(m2, mg);
 
 					for (Arc a : m2.getInArc()) {
 						Mark next = (Mark) a.getSrc();
-						if (!reserved.contains(next)) {
-							novisited.push(next);
-							reserved.add(next);
-						}
+						novisited.push(next);
 					}
 				}
-				if (mg.getMarkSet().size() == 1) {
-					Mark mm = mg.getMarkSet().iterator().next();
-					for (Arc a : mm.getInArc()) {
-						Mark mmnext = (Mark) a.getSrc();
-						MarkGroup mmmg = markToGroup.get(mmnext);
-						if (dagMarkGroup.contains(mmmg)) {
-//								for (Mark cm : mmmg.getMarkSet()) {
-//									mg.add(cm);
-//									this.markToGroup.put(cm, mg);
-//								}
-							mg.addAll(mmmg.getMarkSet());
-							dagMarkGroup.remove(mmmg);
-							allMarkGroup.remove(mmmg);
-						}
-					}
+				if (mg.getMarkSet().size() != 1) {
+					allMarkGroup.add(mg);
+				} else {
+					dagtran.add(mg.getMarkSet().iterator().next());
+					allMarkGroup.add(mg);
 					dagMarkGroup.add(mg);
 				}
-				allMarkGroup.add(mg);
 			}
 		}
+
+//		// DFS 3 pass
+//		novisited.clear();
+//		visited.clear();
+//		Collections.reverse(sorted);
+//		for (Mark m : sorted) {
+//			if (!visited.contains(m) && dagtran.contains(m)) {
+//				MarkGroup mg = new MarkGroup(JSPetriNet.markToString(net, m));
+//				novisited.push(m);
+//				while (!novisited.isEmpty()) {
+//					Mark m2 = novisited.pop();
+//					if (visited.contains(m2) || !dagtran.contains(m2)) {
+//						continue;
+//					}
+//					visited.add(m2);
+//					mg.add(m2);
+//					markToGroup.put(m2, mg);
+//
+//					for (Arc a : m2.getOutArc()) {
+//						Mark next = (Mark) a.getDest();
+//	//					if (!reserved.contains(next)) {
+//							novisited.push(next);
+////							reserved.add(next);
+////						}
+//					}
+//				}
+//				allMarkGroup.add(mg);
+//				dagMarkGroup.add(mg);
+//			}
+//		}
 	}
 	
 	private void connectGroup(MarkGroup src, MarkGroup dest) {
@@ -141,96 +160,4 @@ public class MarkClassAnalysis {
 		}
 		bw.println("}");
 	}
-
-	//	private Map<MarkGroup,Boolean> isSlow;
-//	private Set<Trans> slowTransSet;
-//
-//	private boolean hasFastTrans(Mark m) {
-//		for (Arc a : m.getOutArc()) {
-//			MarkingArc ma = (MarkingArc) a;
-//			if (!slowTransSet.contains(ma.getTrans())) {
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-//
-//	public Set<MarkGroup> searchFastStates(Set<Mark> allState) {
-//		Set<Mark> visited = new HashSet<Mark>();
-//		Set<Mark> determined = new HashSet<Mark>();
-//		Set<Mark> remain = new HashSet<Mark>(allState);
-//		Set<MarkGroup> mgg = new HashSet<MarkGroup>();
-//
-//		MarkGroup slowSet = new MarkGroup("");
-//		for (Mark m : allState) {
-//			if (hasFastTrans(m) == false) {
-//				slowSet.add(m);
-//				visited.add(m);
-//				remain.remove(m);
-//			}
-//		}
-//		isSlow.put(slowSet, true);
-//		mgg.add(slowSet);
-//
-//		while (!remain.isEmpty()) {
-//			Mark start = remain.iterator().next();
-//			MarkGroup mg = createFastGroup(start, remain, visited, determined, allState, slowTransSet, slowSet);
-//			if (mg.size() != 0) {
-//				mgg.add(mg);
-//			}
-//		}
-//		return mgg;
-//	}
-//
-//	private MarkGroup createFastGroup(Mark start, Set<Mark> remain,
-//			Set<Mark> visited, Set<Mark> determined,
-//			Set<Mark> allState, Set<Trans> slowTransSet, MarkGroup slowSet) {
-//		
-//		MarkGroup mg = new MarkGroup("");
-//		LinkedList<Mark> novisited = new LinkedList<Mark>();
-//		novisited.push(start);
-//		while (!novisited.isEmpty()) {
-//			Mark m = novisited.pop();
-//			if (visited.contains(m) || !allState.contains(m)) {
-//				continue;
-//			}
-//			visited.add(m);
-//			remain.remove(m);
-//
-//			for (Arc a : m.getOutArc()) {
-//				MarkingArc ma = (MarkingArc) a;
-//				if (!slowTransSet.contains(ma.getTrans())) {
-//					Mark dest = (Mark) ma.getDest();
-//					if (!determined.contains(dest)) {
-//						if (allState.contains(dest) && !slowSet.contains(dest)) {
-//							mg.add(dest);
-//							determined.add(dest);
-//							novisited.push(dest);
-//						} else {
-//							determined.add(dest);
-////							mg.setTransient(true);
-////							mg.getExitMarkSet().add(m);
-//						}
-//					}
-//				}
-//			}
-//			for (Arc a : m.getInArc()) {
-//				MarkingArc ma = (MarkingArc) a;
-//				if (!slowTransSet.contains(ma.getTrans())) {
-//					Mark src = (Mark) ma.getSrc();
-//					if (!determined.contains(src)) {
-//						if (allState.contains(src)) {
-//							mg.add(src);
-//							determined.add(src);
-//							novisited.push(src);
-//						} else {
-//							// TODO
-//						}
-//					}
-//				}
-//			}
-//		}
-//		return mg;
-//	}
-
 }
