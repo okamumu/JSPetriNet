136c9b7c521829110cb25ba597f77845cfaecfec
Hiroyuki Okamura <okamumu@gmail.com>
2016-11-10 17:48:43 +0900

    diag途中

--- src/jspetrinet/JSPetriNet.java
+++ src/jspetrinet/JSPetriNet.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
