fbb879bcb087bc4141b7ba1a69803f9d2c874819
Hiroyuki Okamura <okamumu@gmail.com>
2016-11-30 10:31:23 +0900

    add examples

--- src/jmatout/SparseMatrixCSCComparator.java
+++ src/jmatout/SparseMatrixCSCComparator.java
@@ -7,15 +7,15 @@ public class SparseMatrixCSCComparator implements Comparator<List<?>> {
 
 	@Override
 	public int compare(List<?> o1, List<?> o2) {
-		int j1 = (int) o1.get(1);
-		int j2 = (int) o2.get(1);
+		int j1 = (Integer) o1.get(1);
+		int j2 = (Integer) o2.get(1);
 		if (j1 < j2) {
 			return -1;
 		} else if (j1 > j2) {
 			return 1;
 		} else {
-			int i1 = (int) o1.get(0);
-			int i2 = (int) o2.get(0);
+			int i1 = (Integer) o1.get(0);
+			int i2 = (Integer) o2.get(0);
 			if (i1 < i2) {
 				return -1;
 			} else if (i1 > i2) {
